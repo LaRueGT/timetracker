d505b874047a31ad1340359fa7c09f06f006b9cc
Garrett LaRue <gtlarue@gmail.com>
2016-08-27 12:32:25 -0400

    added track record type

--- src/timetracker.adb
+++ src/timetracker.adb
@@ -1,18 +1,32 @@
 with Ada.Calendar, Ada.Calendar.Formatting;
 with Ada.Text_IO;
 with Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
-
 use Ada.Calendar, Ada.Calendar.Formatting;
 use Ada.Text_IO;
 use Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
 
 procedure Timetracker is
-   track : Unbounded_String;
-   date : Time := Clock;
+   type Track_Record is
+      record
+         Name : Unbounded_String;
+         Date : Time;
+         Hours : Integer;
+      end record;
+   track : Track_Record;
+
+   procedure Output_Track_Record(track : in Track_Record) is
+   begin
+      Put_Line(track.Name);
+      Put_Line(Image(Date => track.Date));
+      Put_Line(Item => Integer'Image(track.Hours));
+      New_Line;
+   end Output_Track_Record;
+
 begin
    Put_Line("What are you tracking? ");
-   track := Get_Line;
-   Put_Line(track);
-   Put_Line(Image(Date => date));
-   New_Line;
+   track.Name := Get_Line;
+   track.Date := Clock;
+   Put_Line("How mane hours have you done? ");
+   track.Hours := Integer'Value(Get_Line);
+   Output_Track_Record(track);
 end Timetracker;
