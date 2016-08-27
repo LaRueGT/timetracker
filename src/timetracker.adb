c4fac9861175f2ca98da6a56ea116599713a9171
Garrett LaRue <gtlarue@gmail.com>
2016-08-27 12:53:21 -0400

    file output, gitignore

--- src/timetracker.adb
+++ src/timetracker.adb
@@ -1,9 +1,16 @@
+--calendar standard libraries
 with Ada.Calendar, Ada.Calendar.Formatting;
+--text io
 with Ada.Text_IO;
+--unbounded strings
 with Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
+--file IO
+with Ada.Directories, Ada.Streams.Stream_IO;
+
 use Ada.Calendar, Ada.Calendar.Formatting;
 use Ada.Text_IO;
 use Ada.Strings.Unbounded, Ada.Strings.Unbounded.Text_IO;
+use Ada.Directories, Ada.Streams.Stream_IO;
 
 procedure Timetracker is
    type Track_Record is
@@ -13,6 +20,18 @@ procedure Timetracker is
          Hours : Integer;
       end record;
    track : Track_Record;
+
+   filehandle : Ada.Streams.Stream_IO.File_Type;
+   fileaccess : Ada.Streams.Stream_IO.Stream_Access;
+
+   procedure Save_Records_To_File(File : in out Ada.Streams.Stream_IO.File_Type;
+                                  Name : in String ) is
+   begin
+      Create(File, Out_File, Name);
+      fileaccess := Stream(File);
+      Track_Record'Write(fileaccess, track);
+      Close(File);
+   end Save_Records_To_File;
 
    procedure Output_Track_Record(track : in Track_Record) is
    begin
@@ -29,4 +48,5 @@ begin
    Put_Line("How mane hours have you done? ");
    track.Hours := Integer'Value(Get_Line);
    Output_Track_Record(track);
+   Save_Records_To_File(filehandle, "trackingdb");
 end Timetracker;
